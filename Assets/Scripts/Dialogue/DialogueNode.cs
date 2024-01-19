b2dbc69a98f4daf4365ba7823c1e4845779a0ef9
Phuc Hoang <phuc.hoangkien@gmail.com>
2024-01-19 17:41:22 +0700

    Add dialogue scriptable object

---
+++ Assets/Scripts/Dialogue/DialogueNode.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueNode
+{
+    public string uniqueId;
+    public string text;
+    public string[] childNode;
+}

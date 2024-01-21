3c04bd510d58ec8113d53d654402ed35675e1874
Phuc Hoang <phuc.hoangkien@gmail.com>
2024-01-21 10:53:22 +0700

    Add node dragging

--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -9,6 +9,9 @@ public class DialogueEditor : EditorWindow
 {
     Dialogue selectedDialogue;
     GUIStyle nodeStyle;
+    private bool dragging = false;
+    DialogueNode draggingNode = null;
+    private Vector2 offsetPosition;
 
     [MenuItem("Window/Dialogue Editor")]
     public static void ShowEditorWindow()
@@ -56,7 +59,7 @@ public class DialogueEditor : EditorWindow
         }
         else
         {
-            
+            ProcessEvent();
             foreach(DialogueNode item in selectedDialogue.GetAllNodes())
             {
                 OnGUINode(item);
@@ -65,9 +68,40 @@ public class DialogueEditor : EditorWindow
         Repaint();
     }
 
+    private void ProcessEvent()
+    {
+        if (Event.current.type == EventType.MouseDown && draggingNode == null)
+        {
+            draggingNode = GetNodeAtPoint(Event.current.mousePosition);
+            if (draggingNode != null)
+                offsetPosition = draggingNode.rect.position - Event.current.mousePosition;
+        }
+        else if (Event.current.type == EventType.MouseDrag && draggingNode != null)
+        {
+            Undo.RecordObject(selectedDialogue, "Move Dialogue Node");
+            draggingNode.rect.position = Event.current.mousePosition + offsetPosition;
+            GUI.changed = true;
+        }    
+        else if (Event.current.type == EventType.MouseUp && draggingNode != null)
+        {
+            draggingNode = null;
+        }
+    }
+
+    private DialogueNode GetNodeAtPoint(Vector2 point)
+    {
+        DialogueNode foundNode = null;
+        foreach(DialogueNode node in selectedDialogue.GetAllNodes())
+        {
+            if (node.rect.Contains(point))
+                foundNode = node;
+        }
+        return foundNode;
+    }
+
     private void OnGUINode(DialogueNode item)
     {
-        GUILayout.BeginArea(item.position, nodeStyle);
+        GUILayout.BeginArea(item.rect, nodeStyle);
         EditorGUI.BeginChangeCheck();
         EditorGUILayout.LabelField("Node:");
         string newText = EditorGUILayout.TextField(item.text);

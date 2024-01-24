f3ac0335ba85088813f093bb82893ed980cbc921
Phuc Hoang <phuc.hoangkien@gmail.com>
2024-01-24 11:10:04 +0700

    Add next dialogue button logic

--- Assets/Scripts/Player/PlayerConversant.cs
+++ Assets/Scripts/Player/PlayerConversant.cs
@@ -1,14 +1,17 @@
 using RPG.Dialogue;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerConversant : MonoBehaviour
 {
     [SerializeField] Dialogue currentDialogue;
+    DialogueNode currentNode;
+    bool hasNext = true;
     void Start()
     {
-        
+        currentNode = currentDialogue.GetRootNode();
     }
 
     // Update is called once per frame
@@ -21,9 +24,28 @@ public class PlayerConversant : MonoBehaviour
     {
         if (currentDialogue != null)
         {
-            return currentDialogue.GetRootNode().GetText();
+            return currentNode.GetText();
         }
         else
             return "";
+    }
+
+    public void GetNextText()
+    {
+        DialogueNode[] nodes = currentDialogue.GetAllChildren(currentNode).ToArray();
+        if(nodes.Length != 0)
+        {
+            hasNext = true;
+            int randomIndex = Random.Range(0, nodes.Length);
+            currentNode = nodes[randomIndex];
+        }
+        {
+            hasNext = false;
+        }
+    }
+
+    public bool HasNext()
+    {
+        return hasNext;
     }
 }
--- Assets/Scripts/UI/DialogueUI.cs
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -2,15 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueUI : MonoBehaviour
 {
     PlayerConversant playerConversant;
     [SerializeField] TextMeshProUGUI textAI;
+    [SerializeField] Button nextButton;
 
     private void Start()
     {
         playerConversant = GameObject.Find("Player").GetComponent<PlayerConversant>();
+        UpdateTextUI();
+        nextButton.onClick.AddListener(NextButton);
+    }
+
+   
+
+    public void NextButton()
+    {
+        playerConversant.GetNextText();
+        UpdateTextUI();
+    }
+
+    private void UpdateTextUI()
+    {
         textAI.text = playerConversant.GetText();
+        nextButton.gameObject.SetActive(playerConversant.HasNext());
     }
 }

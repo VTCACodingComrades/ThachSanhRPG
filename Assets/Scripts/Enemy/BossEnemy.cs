211f16800124fa7437708c6f9d6a85f2e4379739
Phuc Hoang <phuc.hoangkien@gmail.com>
2024-03-07 18:31:32 +0700

    Add boss enemy attack behavior

--- Assets/Scripts/Enemy/BossEnemy.cs
+++ Assets/Scripts/Enemy/BossEnemy.cs
@@ -6,16 +6,82 @@ public class BossEnemy : Enemy
 {
     private Animator enemyAnimator;
     public AnimatorOverrideController overrideControllers;
+    public Transform target;
+    public float detectRadius = 6;
+    public float attackRadius = 3;
+    public float moveSpeed = 1;
+    private bool isSleeping = true;
+    private Vector3 initialPosition;
+    private float attackCooldown = 1f;
+    private float timeSinceLastAttack;
     // Start is called before the first frame update
     void Start()
     {
+        initialPosition = gameObject.transform.position;
         enemyAnimator = GetComponent<Animator>();
         enemyAnimator.runtimeAnimatorController = overrideControllers;
+        target = GameObject.Find("Player").transform;
+        timeSinceLastAttack = attackCooldown;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isSleeping)
+        {
+            enemyAnimator.SetBool("Walk", false);
+        }
+        if (Vector3.Distance(transform.position, target.position) <= detectRadius)
+        {
+            isSleeping = false;
+            //enemyAnimator.SetBool("WakeUp", true);
+            if (Vector3.Distance(transform.position, target.position) > attackRadius)
+            {
+                //MoveTo(target.position);
+                transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                Vector2 moveDirction = (target.position - transform.position).normalized;
+                if (moveDirction != Vector2.zero)
+                {
+                    
+                    enemyAnimator.SetBool("Walk", true);
+                    enemyAnimator.SetFloat("MoveX", moveDirction.x);
+                    enemyAnimator.SetFloat("MoveY", moveDirction.y);
+                }
+                else
+                {
+                   enemyAnimator.SetBool("Walk", false);
+                }
+            }
+            else
+            {
+                if (timeSinceLastAttack >= 0)
+                {
+                    timeSinceLastAttack -= Time.deltaTime;
+                    enemyAnimator.SetBool("Walk", false);
+                }
+                else
+                {
+                    timeSinceLastAttack = attackCooldown;
+                    enemyAnimator.SetTrigger("Attack");
+                }
+               
+            }
+        }
+        else if (Vector3.Distance(transform.position, target.position) > detectRadius && !isSleeping)
+        {
+            MoveTo(initialPosition);
+            if (Vector3.Distance(transform.position, initialPosition) < 0.01)
+            {
+                isSleeping = true;
+            }
+        }
+    }
+
+    private void MoveTo(Vector3 targetPosition)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        Vector2 moveDirction = (targetPosition - transform.position).normalized;
+        enemyAnimator.SetFloat("MoveX", moveDirction.x);
+        enemyAnimator.SetFloat("MoveY", moveDirction.y);
     }
 }
